f07f834146012266dca2c29bf741caa3dc8a71e5
Parag Karanjekar <98255049+paragkaranjekar@users.noreply.github.com>
2025-10-27 12:14:12 +0530

    Add files via upload

---
+++ helloworld/build/CMakeFiles/HelloWorldData_lib_generate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for HelloWorldData_lib_generate.
+# This may be replaced when dependencies are built.
---
+++ helloworld/build/CMakeFiles/HelloworldPublisher.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloworldPublisher.dir/HelloWorldData.c.o"
+  "CMakeFiles/HelloworldPublisher.dir/HelloWorldData.c.o.d"
+  "CMakeFiles/HelloworldPublisher.dir/publisher.c.o"
+  "CMakeFiles/HelloworldPublisher.dir/publisher.c.o.d"
+  "HelloWorldData.c"
+  "HelloWorldData.h"
+  "HelloworldPublisher"
+  "HelloworldPublisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloworldPublisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
